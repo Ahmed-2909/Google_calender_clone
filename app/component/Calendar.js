13299964176d8dc48d68a04c5bb021faf07208c9
Ahmed <kaziahmed.r@solutelabs.com>
2023-06-24 18:57:48 +0530

    calendar complete

--- app/component/Calendar.js
+++ app/component/Calendar.js
@@ -1,5 +1,5 @@
 "use client"
-import React from 'react'
+import React, { useState } from 'react'
 import FullCalendar from "@fullcalendar/react";
 import dayGridPlugin from "@fullcalendar/daygrid";
 import timeGridPlugin from "@fullcalendar/timegrid";
@@ -7,23 +7,91 @@ import interactionPlugin from "@fullcalendar/interaction";
 
 
 const Calendar = () => {
+    const [Coordinates, setCoordinates] = useState([]);
+    const [formShow, setFormShow] = useState(false)
+    const [events, setEvents] = useState([])
+    const [meeting, setMeeting] = useState("")
+    const [inputValue, setInputValue] = useState("")
+    const [start_end, setStart_end] = useState([])
     const date = new Date();
 
     let day = date.getDate();
     const handleSelect = (info) => {
+        // console.log(info)
         const { start, end } = info;
-        const eventNamePrompt = prompt("Enter Event Name");
-        alert('Coordinates: ' + info.jsEvent.pageX + ',' + info.jsEvent.pageY);
-        if (eventNamePrompt) {
-            const newEvent = {
-                start,
-                end,
-                title: eventNamePrompt,
-                id: new Date().getTime().toString(),
-            };
-            // setEvents([...events, newEvent]);
+        setStart_end([start, end])
+        var days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+        let start_Data_IN_string = JSON.stringify(start);
+        let ending_Data_In_sting = JSON.stringify(end);
+        let GatingData_from_ending_string = ending_Data_In_sting.slice(1, 10)
+        let GatingData_from_starting_string = start_Data_IN_string.slice(1, 10)
+        var d = new Date(GatingData_from_starting_string);
+        var dayName = days[d.getDay()];
+        console.log(dayName);
+        const event = d.toString()
+        let event_split = event.split(" ");
+
+
+
+        event_split[0] = dayName;
+        event_split[4] = GatingData_from_starting_string;
+        event_split[5] = "to";
+        event_split[6] = GatingData_from_ending_string;
+        event_split.splice(7, 8);
+        let meeting_time = event_split.join(' ')
+
+        // console.log(meeting_time);
+
+        setMeeting(meeting_time);
+
+        // start_Data_IN_string.toString()
+
+        // const eventNamePrompt = prompt("Enter Event Name");
+        // alert('Clicked on: ' + info.dateStr);
+        // alert('Coordinates: ' + info.jsEvent.pageX + ',' + info.jsEvent.pageY);
+        let x = info.jsEvent.pageX;
+        if ((x - 500) <= 0) {
+            x = x + 670;
+
+        }
+        setCoordinates([x, info.jsEvent.pageY])
+
+        setFormShow(true);
+
+
+    };
+    const handleEventClick = (selected) => {
+        if (
+            window.confirm(
+                `Are you sure you want to delete the event '${selected.event.title}'`
+            )
+        ) {
+            const eventId = selected.event.id;
+            const updatedEvents = events.filter((event) => event.id !== eventId);
+            setEvents(updatedEvents);
         }
     };
+
+    const submitHandler = () => {
+        if (inputValue.trim().length <= 0) {
+            return;
+        }
+        const newEvent = {
+            start: start_end[0],
+            end: start_end[1],
+            title: inputValue,
+            id: new Date().getTime().toString(),
+        };
+        setEvents([...events, newEvent]);
+        console.log("submit");
+        setInputValue("");
+        setFormShow(false);
+    }
+    const hideHandler = () => {
+        setFormShow(false);
+    }
+    console.log(inputValue);
+
     return (
 
         <div className='w-full sm:w-4/5  mt-3 '>
@@ -31,6 +99,57 @@ const Calendar = () => {
                 <img src={`https://ssl.gstatic.com/calendar/images/dynamiclogo_2020q4/calendar_${day}_2x.png`} className=' w-8 sm:w-9 lg:w-12 ml-3' />
                 <h1>Calendar</h1>
             </div>
+            {formShow && <div className="bg-white rounded-xl shadow-[0_3px_10px_rgb(0,0,0,0.2)]    absolute z-30 w-1/2 lg:w-2/5 overflow-hidden" style={{ top: `${Coordinates[1] - 175}px`, left: `${Coordinates[0] - 670}px` }}>
+                <div className='p-5 '>
+                    <div className='flex p-2 text-xl text-gray-400 justify-between' >
+                        <i class="fi fi-rr-angle-double-small-left"></i>
+                        <div className='hover:text-red-500 hover:text-2xl' onClick={hideHandler}><i class="fi fi-ss-cross-small"></i></div>
+                    </div>
+                    <div className=' w-full flex flex-col items-end'>
+                        <form className='w-4/5 p-2'  >
+                            <input type="text" placeholder='Add Title ' className='bg-transparent w-full  p-1 sm:text-lg lg:text-xl  outline-none border-b-blue-600 border-b-2  my-2' autoFocus={true}
+                                onChange={(e) => setInputValue(e.target.value)} />
+                            <div className='flex justify-between my-2'>
+                                <button className='p-1  rounded-lg  bg-blue-100 text-blue-600  '>Event</button>
+                                <button className='p-1  rounded-lg hover:bg-blue-100 hover:text-blue-600  '>Out of office</button>
+                                <button className='p-1  rounded-lg hover:bg-blue-100 hover:text-blue-600  '>Task</button>
+                                <button className='p-1  rounded-lg hover:bg-blue-100 hover:text-blue-600  '>Appointment</button>
+                            </div>
+                        </form>
+                    </div>
+                    <div className='flex mx-5 items-center my-2 text-gray-500' >
+                        <i class="fi fi-rr-clock-three"></i>
+                        <p className='sm:text-sm  lg:text-lg sm:ml-3 lg:ml-5'>{meeting}</p>
+                    </div>
+                    <div className='flex mx-5 my-2 items-center text-white' >
+                        <i class="fi fi-rr-clock-three" style={{ display: 'hidden' }}></i>
+                        <p className='sm:text-sm cursor-pointer text-blue-500 lg:text-lg sm:ml-3 lg:ml-5'>Find a Time</p>
+                    </div>
+                    <div className='flex mx-5 my-2 items-center text-gray-500' >
+                        <i class="fi fi-rr-users"></i>
+                        <p className='sm:text-sm  lg:text-lg sm:ml-3 lg:ml-5'>Add guests</p>
+                    </div>
+                    <div className='flex mx-5 my-2 items-center ' >
+                        <img className='w-5' src="https://upload.wikimedia.org/wikipedia/commons/thumb/9/9b/Google_Meet_icon_%282020%29.svg/2491px-Google_Meet_icon_%282020%29.svg.png" alt="google meet" />
+                        <p className='sm:text-sm  lg:text-lg sm:ml-3 lg:ml-4 p-2 rounded-md bg-blue-500 text-white'>Add Google Meet Video Conferencing</p>
+                        <div className='p-2 border-l-2 sm:text-sm lg:text-lg  border-l-white rounded-md text-white bg-blue-500'><i class="fi fi-sr-caret-down"></i></div>
+                    </div>
+                    <div className='flex mx-5 my-2 items-center text-gray-500' >
+                        <i class="fi fi-rr-align-left"></i>
+                        <p className='sm:text-sm  lg:text-lg sm:ml-3 lg:ml-5'>Add description or attachments</p>
+                    </div>
+                    <div className='flex mx-5 my-2 items-center text-gray-500' >
+                        <i class="fi fi-br-calendar-day"></i>
+                        <p className='sm:text-sm  lg:text-lg sm:ml-3 lg:ml-5'>kazi Ahmed Raza</p>
+                        <div className='w-3 ml-2 p-2 bg-blue-500  rounded-full'></div>
+                    </div>
+                    <div className='flex justify-end items-center ' onClick={submitHandler}>
+                        <p className='hover:text-gray-500 sm:text-base cursor-pointer mx-2  lg:text-xl '>More option</p>
+                        <button type='submit' className='text-white mx-2 rounded-lg bg-blue-500 hover:bg-blue-600 p-2 sm:text-base cursor-pointer  lg:text-xl '>Save</button>
+                    </div>
+                </div>
+
+            </div>}
             <FullCalendar
                 height="100vh"
                 plugins={[
@@ -53,10 +172,13 @@ const Calendar = () => {
                 nowIndicator={true}
                 navLinks
                 slotDuration="00:60:00"
-                
+                // dateClick={abc}
+                eventResizableFromStart={true}
+                eventOverlap={true}
+                select={handleSelect}
+                eventClick={handleEventClick}
+                events={events}
 
-                  select={handleSelect}
-                //   eventClick={abc}
                 //   eventsSet={(events) => setCurrentEvents(events)}
                 initialEvents={[
                     {
@@ -71,6 +193,7 @@ const Calendar = () => {
                     },
                 ]}
             />
+
         </div>
     )
 }
--- app/component/Navebar.js
+++ app/component/Navebar.js
@@ -18,7 +18,7 @@ const Navebar = () => {
     }
 
     return (
-        <div className=' hidden sm:block sm:w-2/5 lg:w-1/5 '>
+        <div className=' hidden sm:block sm:w-2/5 lg:w-1/5  '>
             <div className='flex items-center border-b-[#DADCE0] border-b-2 sm:lg lg:text-2xl  p-3 gap-2 '>
                 <i class="fi fi-br-menu-burger " ></i>
                 <img src={`https://ssl.gstatic.com/calendar/images/dynamiclogo_2020q4/calendar_${day}_2x.png`} className=' w-6 sm:w-9 lg:w-12 ml-3' />
@@ -43,7 +43,7 @@ const Navebar = () => {
                 </div>}
                 <div className=''>
                     <FullCalendar
-                       height="51vh"
+                        height="51vh"
                         plugins={[
                             dayGridPlugin,
                         ]}
@@ -53,6 +53,40 @@ const Navebar = () => {
                             right: "",
                         }}
                     />
+                </div>
+                <div className='m-2 rounded-sm'>
+                    <input type="text" placeholder='Search for People...' className='p-2 bg-gray-200 rounded-md outline-none' />
+                </div>
+                <div className='my-2 mx-4 flex flex-col'>
+                    <div className='flex justify-between mb-3'>
+                        <h2>My calendar</h2>
+                        <i class="fi fi-bs-angle-up"></i>
+                    </div>
+                    <label>
+                        <input type="checkbox" id="Checkbox1" defaultChecked={true} className='text-red-500 mx-2' />
+                        Kazi Ahmed Raza
+                    </label>
+                    <label>
+                        <input type="checkbox" id="Checkbox2" defaultChecked={true} className='text-red-500 mx-2' />
+                        Birth day
+                    </label>
+                    <label>
+                        <input type="checkbox" id="Checkbox3" defaultChecked={true} className='text-red-500 mx-2' />
+                        Task
+                    </label>
+                </div>
+                <div className='mt-1 mx-4 flex flex-col'>
+                    <div className='flex justify-between mb-1'>
+                        <h2>other calendar</h2>
+                        <div className='flex w-1/5 justify-evenly'>
+                            <i class="fi fi-bs-angle-up"></i>
+                            <i class="fi fi-bs-plus-small"></i>
+                        </div>
+                    </div>
+                    <label>
+                        <input type="checkbox" id="Checkbox4" defaultChecked={true} className='text-red-500 mx-2' />
+                        Holidays in India
+                    </label>
                 </div>
             </div>
         </div>
